bf1bda59425de5d0ea19b805ba409df551dd6e6e
Bostaji Thomas <thomas.bostaji@student.vinci.be>
2021-11-29 19:41:43 +0100

    add function -name: simpleMinutes

--- BerlinClockKata.php
+++ BerlinClockKata.php
@@ -1,6 +1,23 @@
 <?php
 
+namespace BerlinClockKata;
+
 class BerlinClockKata
 {
+    public function simpleMinutes(int $minutes): string
+    {
+
+        $minutes = $minutes % 5;
+        $reponse = "";
+        for ($i = 1; $i <= 4; $i++) {
+            if ($i <= $minutes) {
+                $reponse .= "Y";
+            } else if ($i > $minutes) {
+                $reponse .= "O";
+            }
+        }
+        return $reponse;
+
+    }
 
 }
