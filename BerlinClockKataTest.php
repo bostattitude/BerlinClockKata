5cf39af70961858295b15c64b4571ba31434604e
Bostaji Thomas <thomas.bostaji@student.vinci.be>
2021-11-29 19:51:37 +0100

    add test:  test_simpleMinutes_given1_ShouldReturnYOOO

--- BerlinClockKataTest.php
+++ BerlinClockKataTest.php
@@ -1,9 +1,27 @@
 <?php
 
 
+require "vendor/autoload.php";
+require "BerlinClockKata.php";
+
+use BerlinClockKata\BerlinClockKata;
 use PHPUnit\Framework\TestCase;
 
 class BerlinClockKataTest extends TestCase
 {
+
+    private $berlinClockKata;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->berlinClockKata = new BerlinClockKata();
+    }
+
+    public function test_simpleMinutes_given1_ShouldReturnYOOO()
+    {
+        $actual = $this->berlinClockKata->simpleMinutes(1);
+        $this->assertEquals("YOOO", $actual);
+    }
 
 }
